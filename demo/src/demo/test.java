3eb2b5fd0b5117b152dc7a6a70feee96deac8da9
fdm <fdm>
2018-09-04 21:26:20 +0800

    121

--- demo/src/demo/test.java
+++ demo/src/demo/test.java
@@ -1,13 +1,7 @@
 package demo;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class test {
 	public static void main(String[] args) {
-		Scanner str = new Scanner(System.in);
-		String string = str.nextLine();
-		String strig = str.nextLine();
+		System.out.println(12);
 	}
 }
