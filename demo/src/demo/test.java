9a815995a41daf6d9a681d62a1c9e271650b4ce5
fdm <fdm@163.com>
2018-09-04 19:48:56 +0800

    test

---
+++ demo/src/demo/test.java
@@ -0,0 +1,12 @@
+package demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class test {
+	public static void main(String[] args) {
+		Scanner str = new Scanner(System.in);
+		String string = str.nextLine();
+	}
+}
